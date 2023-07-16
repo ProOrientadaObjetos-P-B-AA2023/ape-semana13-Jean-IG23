86c261294cb598696544227d848b3f1aa83024ff
Jean-IG23 <116389739+Jean-IG23@users.noreply.github.com>
2023-07-15 23:41:08 -0500

    Update Principal.java

--- problema01/src/paquete005/Principal.java
+++ problema01/src/paquete005/Principal.java
@@ -4,7 +4,16 @@
  */
 package paquete005;
 
+import paquete001.Persona;
+import paquete002.Ciudad;
 import paquete003.BilleteraPagos;
+import paquete004.*;
+import paquete006.DatosAgua;
+import paquete006.DatosLuz;
+import paquete006.DatosPropiedades;
+import paquete006.DatosTelefono;
+
+import java.util.ArrayList;
 
 /**
  *
@@ -12,7 +21,56 @@ import paquete003.BilleteraPagos;
  */
 public class Principal {
     public static void main(String[] args) {
-        BilleteraPagos pago = new BilleteraPagos();
-        System.out.println(pago);
+        BilleteraPagos billetera = new BilleteraPagos();
+        ArrayList<PagoAguaPotable> pagoAgua = new ArrayList<>();
+        ArrayList<PagoLuzElectrica> pagoLuz = new ArrayList<>();
+        ArrayList<PagoPredial> predio = new ArrayList<>();
+        ArrayList<PagoTelefonoConvencional> telefono = new ArrayList<>();
+
+        double[][] AguaCasa = new DatosAgua().datosCasas();
+        for (int i = 0; i < AguaCasa.length; i++) {
+            pagoAgua.add(new PagoAguaPotable("Casa", AguaCasa[i][0],AguaCasa[i][1],AguaCasa[i][2]));
+        }
+        double[][] AguaComercial = new DatosAgua().datosComerciales();
+        for (int i = 0; i < AguaComercial.length; i++) {
+            pagoAgua.add(new PagoAguaPotable("Comercial", AguaComercial[i][0],AguaComercial[i][1],AguaComercial[i][2]));
+        }
+        double[][] LuzCasa = new DatosLuz().datosLoja();
+        Ciudad ci = new Ciudad("Loja");
+        for (int i = 0; i < LuzCasa.length; i++) {
+            pagoLuz.add(new PagoLuzElectrica(ci, LuzCasa[i][0],LuzCasa[i][1],LuzCasa[i][2]));
+        }
+        double[][] LuzComercio = new DatosLuz().datosGeneral();
+        Ciudad ci1 = new Ciudad("Quito");
+        for (int i = 0; i < LuzComercio.length; i++) {
+            pagoLuz.add(new PagoLuzElectrica(ci1, LuzComercio[i][0],LuzComercio[i][1], LuzComercio[i][2]));
+        }
+        double[][] Propiedades = new DatosPropiedades().datos();
+        for (int i = 0; i < Propiedades.length; i++) {
+            predio.add(new PagoPredial(Propiedades[i][0],Propiedades[i][1]));
+        }
+        double[][] Telefono = new DatosTelefono().datos();
+        for (int i = 0; i < Telefono.length; i++) {
+            telefono.add(new PagoTelefonoConvencional(Telefono[i][0],Telefono[i][1],Telefono[i][2]));
+        }
+
+        CalcularAPagar1 cal = new CalcularAPagar1(pagoAgua,pagoLuz,predio,telefono);
+        Persona per = new Persona("Jean", "Iñiguez", 19,"1105727992",ci);
+        billetera = new BilleteraPagos(per,"Julio",pagoAgua,pagoLuz,predio,telefono,cal.calcularAPagar1());
+        System.out.println(billetera);
+
+
+            for (Pagos pago : pagoAgua) {
+            pago.calcularPagos();
+        }
+        for (Pagos pago : pagoLuz) {
+            pago.calcularPagos();
+        }
+        for (Pagos pago : predio) {
+            pago.calcularPagos();
+        }
+        for (Pagos pago : telefono) {
+            pago.calcularPagos();
+        }
     }
 }
