daf79de77d16f1db909ee9929c08b3c5d0e8778f
Jean-IG23 <116389739+Jean-IG23@users.noreply.github.com>
2023-07-15 23:38:33 -0500

    Update BilleteraPagos.java

--- problema01/src/paquete003/BilleteraPagos.java
+++ problema01/src/paquete003/BilleteraPagos.java
@@ -10,29 +10,40 @@ import paquete004.PagoLuzElectrica;
 import paquete004.PagoPredial;
 import paquete004.PagoTelefonoConvencional;
 
-/**
- *
- * @author reroes
- */
+import java.util.ArrayList;
+
 public class BilleteraPagos {
     public Persona persona;
-    public double gastoPagos;
     public String mes;
-    public PagoAguaPotable aguaCasa;
-    public PagoAguaPotable aguaComercio;
-    public PagoLuzElectrica luzCasa;
-    public PagoLuzElectrica luzComercio;
-    public PagoPredial casa1;
-    public PagoPredial casa2;
-    public PagoTelefonoConvencional telefonoCasa;
-    public PagoTelefonoConvencional telefonoFinca;
-    
-    public String toString(){
-        /*
-            Se debe presentar el reporte que incluya
-            información correspondiente oportuna
-        */
-        return "Presentar Reporte";
+    public ArrayList<PagoAguaPotable> aguaPotable;
+    public ArrayList<PagoLuzElectrica> luzElectrica;
+    public ArrayList<PagoPredial> predio;
+    public ArrayList<PagoTelefonoConvencional> telefono;
+    public double gastoPagos;
+
+    public BilleteraPagos(Persona persona, String mes, ArrayList<PagoAguaPotable> aguaPotable, ArrayList<PagoLuzElectrica> luzElectrica, ArrayList<PagoPredial> predio, ArrayList<PagoTelefonoConvencional> telefono,double gastoPagos) {
+        this.persona = persona;
+        this.mes = mes;
+        this.aguaPotable = aguaPotable;
+        this.luzElectrica = luzElectrica;
+        this.predio = predio;
+        this.telefono = telefono;
+        this.gastoPagos = gastoPagos;
     }
-    
+
+    public BilleteraPagos(){
+    }
+
+    @Override
+    public String toString() {
+        return "BilleteraPagos{" +
+                "persona=" + persona +
+                ", gastoPagos=" + gastoPagos +
+                ", mes='" + mes + '\'' +
+                ", aguaPotable=" + aguaPotable +
+                ", luzElectrica=" + luzElectrica +
+                ", predio=" + predio +
+                ", telefono=" + telefono +
+                '}';
+    }
 }
