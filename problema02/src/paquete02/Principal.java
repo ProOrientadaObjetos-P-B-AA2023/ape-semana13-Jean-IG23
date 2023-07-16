9bdd84b18a5b3f1746d3860c9e8a4bc36f9af426
Jean-IG23 <116389739+Jean-IG23@users.noreply.github.com>
2023-07-16 12:16:07 -0500

    Update Principal.java

--- problema02/src/paquete02/Principal.java
+++ problema02/src/paquete02/Principal.java
@@ -1,40 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package paquete02;
 
 import java.util.ArrayList;
 import paquete01.Televisor;
+import paquete03.VentasTvs;
 
-/**
- *
- * @author reroes
- */
 public class Principal {
-    
+
     public static void main(String[] args) {
-       
-        Televisor t1 = new Televisor();
-        t1.establecerMarca("LG-14 pulgadas");
-        t1.establecerPrecio(300.2);
+        ArrayList<Televisor> televisores = new ArrayList<>();
+        televisores.add(new Televisor("LG-14 pulgadas", 300.2));
+        televisores.add(new Televisor("SAMSUNG-21 pulgadas", 1300.2));
+        televisores.add(new Televisor("RIVIERA-29 pulgadas", 2300.5));
 
-        Televisor t2 = new Televisor();
-        t2.establecerMarca("SAMSUMG-21 pulgadas");
-        t2.establecerPrecio(1300.2);
-
-        Televisor t3 = new Televisor();
-        t3.establecerMarca("RIVIERA-29 pulgadas");
-        t3.establecerPrecio(2300.5);
-
-        ArrayList<Televisor> tvs = new ArrayList<>();
-        tvs.add(t1);
-        tvs.add(t2);
-        tvs.add(t3);
         System.out.println("-----------------");
-        System.out.printf("%.2f\n", t1.totalPrecioTvs(tvs));
-        // System.out.printf("%s\n", t1.listaMarcasVendidas(tvs));
+        for (Televisor televisor : televisores) {
+            System.out.println(televisor);
+        }
 
+        VentasTvs ventas = new VentasTvs(televisores);
+        System.out.println("Televisor más caro: " + ventas.obtenerTelevisorMasCaro());
+        System.out.println("Precio Total: " + ventas.obtenerPrecioTotal());
+        System.out.println("Marcas Vendidas: \n" + ventas.obtenerMarcasVendidas());
     }
 }
